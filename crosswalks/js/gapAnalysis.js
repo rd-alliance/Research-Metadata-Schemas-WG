ea69d5d374f753aade3d430bb25271305fdd8f80
ito-coop <59710218+ito-coop@users.noreply.github.com>
2020-04-21 13:52:43 -0700

    Added gap analysis visualization

---
+++ crosswalks/js/gapAnalysis.js
@@ -0,0 +1,30 @@
+$.getJSON("https://ito-coop.github.io/SchemaCrosswalks/json/gapAnalysis.json", gapAnalysis);
+function gapAnalysis(data){
+var chart = am4core.create("gapAnalysis", am4charts.TreeMap);
+chart.data = data;
+chart.layoutAlgorithm = chart.binaryTree;
+/* Set color step */
+chart.colors.step = 2;
+
+/* Define data fields */
+chart.dataFields.value = "value";
+chart.dataFields.name = "name";
+
+/* Configure top-level series */
+var level1 = chart.seriesTemplates.create("0");
+var level1_column = level1.columns.template;
+level1_column.column.cornerRadius(10, 10, 10, 10);
+level1_column.fillOpacity = 0.8;
+level1_column.stroke = am4core.color("#fff");
+level1_column.strokeWidth = 5;
+level1_column.strokeOpacity = 1;
+
+/* Add bullet labels */
+var level1_bullet = level1.bullets.push(new am4charts.LabelBullet());
+level1_bullet.locationY = 0.5;
+level1_bullet.locationX = 0.5;
+level1_bullet.label.text = "{name}";
+level1_bullet.label.fill = am4core.color("#fff");
+
+
+}
